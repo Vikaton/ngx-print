3955d262b638937ba315e6b064a9961f3204a316
Andre Engelbrecht <andre@modsi.io>
2020-05-14 15:48:27 -0500

    update: Set popup window width & height explicitly

--- src/lib/ngx-print.directive.ts
+++ src/lib/ngx-print.directive.ts
@@ -124,7 +124,8 @@ public returnStyleValues() {
     }
 
     printContents = document.getElementById(this.printSectionId).innerHTML;
-    popupWin = window.open("", "_blank", "top=0,left=0,height=auto,width=auto");
+    // popupWin = window.open("", "_blank", "top=0,left=0,height=auto,width=auto");
+    popupWin = window.open("", `_blank", "top=0,left=0,height=${screen.height * 0.8},width=${screen.width * 0.8}`);
     popupWin.document.open();
     popupWin.document.write(`
       <html>
